12afcad70ef59adcaf11e1847e471a5817442849
kitsuyui <kitsuyui@kitsuyui.com>
2023-09-13 01:59:12 +0900

    Install gibo

--- src/gibo.rs
+++ src/gibo.rs
@@ -3,6 +3,7 @@ pub fn gibo_command(target: &str) -> std::io::Result<String> {
         .arg("dump")
         .arg(target)
         .output()?;
+
     let stdout = match String::from_utf8(output.stdout) {
         Ok(it) => it,
         Err(err) => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, err)),
